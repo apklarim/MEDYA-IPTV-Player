a374b9c02532ea3358a2d6de4cdd24e63323fae6
apklarim <apklarim@gmail.com>
2026-06-17 21:54:46 +0000

    Live TV layout fixed

--- app/src/main/java/com/medya/iptvplayer/LiveTvActivity.kt
+++ app/src/main/java/com/medya/iptvplayer/LiveTvActivity.kt
@@ -1,18 +1,12 @@
 package com.medya.iptvplayer
 
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class LiveTvActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val tv = TextView(this)
-        tv.text = "CANLI TV EKRANI"
-        tv.textSize = 24f
-
-        setContentView(tv)
+        setContentView(R.layout.activity_live_tv)
     }
 }
